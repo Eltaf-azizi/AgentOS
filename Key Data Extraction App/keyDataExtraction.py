dfedce42a9a5fb3a04036b8916ac3f5944226fdd
Eltaf-azizi <iltfhuss@gmail.com>
2025-06-10 23:30:38 +0500

    working on the key data extraction techniques

--- Key Data Extraction App/keyDataExtraction.py
+++ Key Data Extraction App/keyDataExtraction.py
@@ -19,3 +19,24 @@ class Person(BaseModel):
     # ^ Doc-string for the entity Person.
     # This doc-string is sent to the LLM as the description of the schema Person,
     # and it can help to improve extraction results.
+
+
+    # Note that:
+    # 1. Each field is an `optional` -- this allows the model to decline to extract it!
+    # 2. Each field has a `description` -- this description is used by the LLM.
+    # Having a good description can help improve extraction results.
+    name: Optional[str] = Field(default=None, description="The name of the person")
+    lastname: Optional[str] = Field(
+        default=None, description="The lastname of the person if known"
+    )
+
+
+    country: Optional[str] = Field(
+        default=None, description="The country of the person if known"
+    )
+
+
+from typing import Optional
+
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.pydantic_v1 import BaseModel, Field
