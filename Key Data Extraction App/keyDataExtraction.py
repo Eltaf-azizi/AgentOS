54e0b5e6a4b69896ba22d9652232374c4886be16
Eltaf-azizi <iltfhuss@gmail.com>
2025-06-10 12:39:33 +0500

    working on the class person

--- Key Data Extraction App/keyDataExtraction.py
+++ Key Data Extraction App/keyDataExtraction.py
@@ -2,3 +2,20 @@ import os
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())
 openai_api_key = os.environ["OPENAI_API_KEY"]
+
+
+from langchain_openai import ChatOpenAI
+
+llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
+
+from typing import Optional
+
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+
+class Person(BaseModel):
+    """Information about a person."""
+
+    # ^ Doc-string for the entity Person.
+    # This doc-string is sent to the LLM as the description of the schema Person,
+    # and it can help to improve extraction results.
