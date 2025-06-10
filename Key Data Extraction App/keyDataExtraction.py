3b1e0bcebe08fd41e53607083049b6f74d56080f
Eltaf-azizi <iltfhuss@gmail.com>
2025-06-10 23:42:05 +0500

    defining a custom prompt to provide instruction and any additional context

--- Key Data Extraction App/keyDataExtraction.py
+++ Key Data Extraction App/keyDataExtraction.py
@@ -40,3 +40,24 @@ from typing import Optional
 
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.pydantic_v1 import BaseModel, Field
+
+# Define a custom prompt to provide instruction and any additional context.
+# 1) You can add examples into the prompt template to improve extraction quality
+# 2) Introduce additional parameters to take context into account (e.g, include metadata
+#    about the document from which the text was extracted.)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are an expert extraction algorithm. "
+            "Only extract relevant information from the text. "
+            "If you do not know the value of an attribute asked to extract, "
+            "return null for the attribute's value.",
+        ),
+        ("human", "{text}"),
+    ]
+)
+
+
+chain = prompt | llm.with_structured_output(schema=Person)
