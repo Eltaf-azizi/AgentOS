449d2da266790baea84f0b42561a5736f5ad5e76
Eltaf-azizi <iltfhuss@gmail.com>
2025-06-12 19:20:37 +0500

    working on the system prompt

--- QA over PDF file/qa-from-pdf.py
+++ QA over PDF file/qa-from-pdf.py
@@ -33,3 +33,23 @@ retriever = vectorstore.as_retriever()
 
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+
+
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't lnow. Use three sentences maximum and keep the "
+    "answer consice."
+    "\n\n"
+    "{context}"
+)
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
